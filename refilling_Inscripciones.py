b928a0a3159fc7e7d92908ada29d20a75e70776e
Diego <dilogi7@gmail.com>
2018-01-13 21:18:49 -0500

    xd

--- refilling_Inscripciones.py
+++ refilling_Inscripciones.py
@@ -16,9 +16,10 @@ headers = {
     "Accept-Language":"es-ES,es;q=0.9,en;q=0.8"
 }
 cont = 0
-def make_request():
-    global cont
-    while True :
+def make_request(hilo,left,right):
+    cont=0
+    dni=left
+    while left<right:
         try:
             r = requests.get("http://www.inscripciones.uni.edu.pe/login");
         except ValueError:
@@ -27,7 +28,8 @@ def make_request():
         text = r.text
         soup = BeautifulSoup(text , features = "lxml").body
         token = soup.find_all('input')[0]['value']
-        dni_str = "7"+"{num:07d}".format(num=random.randint(1,2900000)) # people from Peru [16> years] now have a ID starting with 7 I think
+        #dni_str = "7"+"{num:07d}".format(num=random.randint(1,2900000)) # people from Peru [16> years] now have a ID starting with 7 I think
+        dni_str = str(left)
         len_password = random.randint(8,15)
         password = hashlib.sha256(str(random.randint(1000000000,9999999999)).encode()).hexdigest()[:len_password] # Making difficult to find wich one is fake
 
@@ -42,17 +44,20 @@ def make_request():
             if r.status_code==200:
                 cont+=1
                 if cont%50==0:
-                    print("\r{:d} requests enviados".format(cont),end='')
+                    print("\r{:d} requests enviados - hilo n°{:d}:{:f}% lastOne:{:d}\n".format(cont,hilo,cont*100/(right-left),left),end='')
                     time.sleep(0.200)
+                left+=1
             else:
                 print("Error:", r.status_code)
         except ValueError:
             print("Error: retrying http post")
             time.sleep(2)
-Nhilos = 4
+Nhilos = 10
 hilos = []
+inicio = 70000000
+rango = 500000
 for i in range(Nhilos):
-    hilos.append(threading.Thread(target=make_request))
+    hilos.append(threading.Thread(target=make_request,args=[i+1,inicio+i*rango,inicio+(i+1)*rango]))
     print("Comenzando hilo:",i+1)
     hilos[i].start()
 for i in range(Nhilos):
